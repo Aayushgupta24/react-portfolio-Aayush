874c28e5f7ca5be4f517ba1129d63353551ba280
Aayush Kumar Gupta <aayushkumargupta@Aayushs-MacBook-Air.local>
2025-11-10 22:38:55 +0530

    added new

--- src/constants/index.js
+++ src/constants/index.js
@@ -9,29 +9,29 @@ export const ABOUT_TEXT = `I am a dedicated and versatile Software Engineer with
   I have a strong foundation in problem-solving, evidenced by my 4-star rating on Codechef and a 1935+ rating on LeetCode, where I have solved over 2000 problems. 
   My technical expertise includes full-stack development using the MERN stack, I have worked with a variety of technologies, including React.js, Node.js, Express.js, MySQL, MongoDB, System Design, REST API's, AWS. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 export const EXPERIENCES = [
+  // {
+  //   year: "2025 (March) – Present",
+  //   role: "Full-stack Engineer",
+  //   company: "DBA Growth Pods",
+  //   description: `Developed and maintained CI/CD pipelines, transforming a monolithic architecture to microservices for improved scalability.
+  // Optimized Jenkins pipelines for seamless microservices activation and enhanced deployment efficiency.
+  // Developed an AI-powered co-pilot for a payroll application using OpenAI’s LLM, enhancing UX/UI through intelligent assistance.
+  // Designed and developed microservices and exposed functionalities as RESTful APIs. Collaborated with front-end web and mobile engineers for API integration.
+  // Produced quality backend deliverables using Node.js, Express.js, MongoDB, REST APIs, and AWS.`,
+  //   technologies: [
+  //     "Node.js",
+  //     "Express.js",
+  //     "REST API",
+  //     "MongoDB",
+  //     "AWS",
+  //     "CI/CD",
+  //     "Jenkins",
+  //     "OpenAI API",
+  //     "System Design",
+  //   ],
+  // },
   {
-    year: "2025 (March) – Present",
-    role: "Full-stack Engineer",
-    company: "DBA Growth Pods",
-    description: `Developed and maintained CI/CD pipelines, transforming a monolithic architecture to microservices for improved scalability.
-  Optimized Jenkins pipelines for seamless microservices activation and enhanced deployment efficiency.
-  Developed an AI-powered co-pilot for a payroll application using OpenAI’s LLM, enhancing UX/UI through intelligent assistance.
-  Designed and developed microservices and exposed functionalities as RESTful APIs. Collaborated with front-end web and mobile engineers for API integration.
-  Produced quality backend deliverables using Node.js, Express.js, MongoDB, REST APIs, and AWS.`,
-    technologies: [
-      "Node.js",
-      "Express.js",
-      "REST API",
-      "MongoDB",
-      "AWS",
-      "CI/CD",
-      "Jenkins",
-      "OpenAI API",
-      "System Design",
-    ],
-  },
-  {
-    year: "2024 (February) – 2025 (January)",
+    year: "2024 (May) – Present",
     role: "Software Engineer",
     company: "TechPyro",
     description: `Produced comprehensive API documentation using Swagger, making information accessible for developers and stakeholders.
